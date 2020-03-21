c3391ddac3c1f76b3c7ebb8f8ac92fa08aa486e7
Magda Ciezka <polhrub@interia.pl>
2020-03-21 19:31:25 +0100

    First commit

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module SortingInternation {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextField elements;
+    @FXML
+    Button sort;
+}
