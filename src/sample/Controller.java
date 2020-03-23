40a43e1cb8eb48e9aac65f53d9170ce653dbabd1
Magda Ciezka <polhrub@interia.pl>
2020-03-23 17:51:30 +0100

    Add GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,38 @@
 package sample;
 
-import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import sorting.FloatElement;
 
-public class Controller {
-    @FXML
-    TextField elements;
-    @FXML
-    Button sort;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+   ObservableList<String> algorithms= FXCollections.observableArrayList("Pigeonhole sort","Insertion sort","Merge sort");
+   public List<FloatElement> list =new ArrayList<>();
+   public Button add;
+   public Button sort;
+   public TextField addName;
+   public TextField addValue;
+   public ComboBox algorithm;
+   public void addElement(){
+      FloatElement element = new FloatElement(addName.getText(),Float.parseFloat(addValue.getText()));
+      list.add(element);
+   }
+
+   public void sort(){
+
+   }
+
+   @Override
+   public void initialize(URL url, ResourceBundle resourceBundle) {
+      algorithm.setItems(algorithms);
+
+   }
 }
